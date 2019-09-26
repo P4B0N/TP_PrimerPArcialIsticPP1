fcb52fefd0ca7e89bab946a269833381a3f3cdab
PALUBO <54085980+P4B0N@users.noreply.github.com>
2019-09-26 08:30:33 -0300

    Add files via upload

---
+++ Bonaduce.Pablo/HacerIngreso.php
@@ -0,0 +1,10 @@
+<?php
+$MiObjeto= new stdClass();
+$MiObjeto->Patente=$_GET['patente'];
+$MiObjeto->Hora=$_GET['hora'];
+$archivo=fopen('estacionados.txt', 'a');
+fwrite($archivo,json_encode($MiObjeto)."\n");
+fclose($archivo);
+
+header("Location: okIng.php");
+?>
